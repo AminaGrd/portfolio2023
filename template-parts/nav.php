b1dd623bf37d11080eb2c76dc32ee9ffa7230f88
Adrien-Ray <a.raymond@codeur.online>
2022-03-02 14:36:35 +0100

    ico site dynamic with backoffice

--- template-parts/nav.php
+++ template-parts/nav.php
@@ -1,5 +1,5 @@
 <!-- <nav>
-    <div class="nav__logo"><img onclick="location.href='<?php bloginfo('wpurl'); ?>';" src="<?php echo bloginfo('template_url'); ?>/logo.png" alt="icone du site"></div>
+    <div class="nav__logo"><img onclick="location.href='<?php bloginfo('wpurl'); ?>';" src="<?php echo get_site_icon_url(); ?>" alt="icone du site"></div>
     <?php wp_nav_menu( array( 'menu' => 'navbar' ) ); ?>
     <div class="nav__burger" id="burger">
         <div></div>
