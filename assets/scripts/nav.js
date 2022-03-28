aea3f6bde5f9831e899c9a8b3e846522cfef3bf1
Adrien-Ray <a.raymond@codeur.online>
2022-03-28 12:00:34 +0200

    menu in mobile disparait when click on

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -1,17 +1,27 @@
 // display responsive menu
+/* 
+const burger = document.getElementById('burger');
+const inMenu = document.querySelector("nav ul.menu");
+const navbarlanding = document.getElementsByClassName('menu-menuName-container')[0]; // cible nav wp list
+let i = 0;
 
-//const burger = document.getElementById('burger');
-//const navMenu = document.getElementsByClassName('menu-navbar-container')[0]; // cible nav wp list
-//let i = 0;
-//burger.addEventListener('click', function () {
-//    i++;
-//    navMenu.classList.toggle('menu-navbar-container--show');
-//    if (i>=2) {
-//        navMenu.classList.toggle('menu-navbar-container--unshow');
-//    }
-//})
+burger.addEventListener('click', function() {
+    toogleMenu();
+})
+inMenu.addEventListener('click', function() { // si on clique sur le menu
+    if (i != 0) {
+        toogleMenu();
+    }
+})
 
-
+function toogleMenu() {
+    i++;
+    navbarlanding.classList.toggle('menu-navbar-container--show');
+    if (i >= 2) {
+        navbarlanding.classList.toggle('menu-navbar-container--unshow');
+    }
+}
+ */
 // definition de placeholder search input, par exemple pour ne pas payer le premium du plugin "ivory search"
 
 // document.getElementById('is-search-input-53').setAttribute('placeholder', 'Rechercher');
