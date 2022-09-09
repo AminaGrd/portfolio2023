c60cc7264754fa67f959b2a00be01cb7fd238b71
Adrien-Ray <a.raymond@codeur.online>
2022-09-09 10:45:36 +0200

    style content.css

--- functions.php
+++ functions.php
@@ -19,6 +19,13 @@ function register_assets(){
           array(),
           '1.0'
     );
+	
+    wp_enqueue_style(
+          'content-css',
+          get_template_directory_uri().'/assets/styles/content.css',
+          array(),
+          '1.0'
+    );
 
 }
 add_action('wp_enqueue_scripts', 'register_assets');
