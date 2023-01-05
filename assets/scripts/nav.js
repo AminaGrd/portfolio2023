5dfbefa157f5fbce863726e1e17669d537346d76
Amina Grd <amina.giraud@gmail.com>
2023-01-05 16:44:11 +0100

    finish navbar desktop and mobile whith burger

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -1,16 +1,19 @@
 // display responsive menu
-/* 
+
 const burger = document.getElementById('burger');
 const inMenu = document.querySelector("nav ul.menu");
-const navbarlanding = document.getElementsByClassName('menu-menuName-container')[0]; // cible nav wp list
+const navbarlanding = document.getElementsByClassName('menu-navbar-container')[0]; // cible nav wp list
+const menuburger = document.getElementsByClassName('nav__burger')[0];
 let i = 0;
 
 burger.addEventListener('click', function() {
     toogleMenu();
+    crossMenu();
 })
 inMenu.addEventListener('click', function() { // si on clique sur le menu
     if (i != 0) {
         toogleMenu();
+        crossMenu();
     }
 })
 
@@ -21,7 +24,14 @@ function toogleMenu() {
         navbarlanding.classList.toggle('menu-navbar-container--unshow');
     }
 }
- */
+function crossMenu() {
+    i++;
+    menuburger.classList.toggle('nav__burger--cross');
+    if (i >= 3) {
+        menuburger.classList.toggle('nav__burger--burger');
+    }
+}
+
 // definition de placeholder search input, par exemple pour ne pas payer le premium du plugin "ivory search"
 
 // document.getElementById('is-search-input-53').setAttribute('placeholder', 'Rechercher');
