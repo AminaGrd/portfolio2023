e81480a2b964cae184551fde1d615855983724f1
Amina Grd <amina.giraud@gmail.com>
2023-01-07 13:25:02 +0100

    beginning footer

--- functions.php
+++ functions.php
@@ -34,6 +34,13 @@ function register_assets(){
       '1.0'
 );
 
+      wp_enqueue_style(
+            'footer-css',
+            get_template_directory_uri().'/assets/styles/footer.css',
+            array(),
+            '1.0'
+      );
+
 }
 add_action('wp_enqueue_scripts', 'register_assets');
 
