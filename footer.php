9cc0ed2e54196a4834dba5afc603495dfde2abca
Amina Grd <amina.giraud@gmail.com>
2023-01-07 14:30:34 +0100

    finish css footer

--- footer.php
+++ footer.php
@@ -1,6 +1,10 @@
     </div><!-- fin div.content -->
     <footer>
-        <!-- <?php wp_nav_menu( array( 'menu' => 'footer' ) ); ?> -->
+        <div class="footer__contact">
+            <a href="mailto:amina.giraud@gmail.com"><img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/footer/ico-mail.svg" alt="icône mail"></a>
+            <a href="tel:0658195213"><img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/footer/ico-phone.svg" alt="icône téléphone"></a>
+
+        </div>
     </footer>
     <script src="<?php bloginfo('template_directory'); ?>/assets/scripts/nav.js"></script>
 </body>
