69aa85d45a3c2ef9aec87ea8514d50078046a1d2
Amina Grd <amina.giraud@gmail.com>
2023-03-27 17:37:09 +0200

    begin page contact

--- 404.php
+++ 404.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?> 
 <h1>Page non trouvée...</h1>
-<p>Veuillez revenir à l'accueil.</p>
-<a href="<?php bloginfo('wpurl'); ?>">Home</a>
+<p class="error-text">Veuillez revenir à l'accueil.</p>
+<a href="<?php bloginfo('wpurl'); ?>" class="error-return-home">Accueil</a>
 <?php get_footer(); ?>
